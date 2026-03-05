e4834c8357d50f4b1a1fb9e283b3abd06b47dd9c
Eccentrichealerbuilds <healercpp@gmail.com>
2026-03-05 21:21:47 +0100

    adjust rate limit request

--- faucet-backend/src/middleware.rs
+++ faucet-backend/src/middleware.rs
@@ -7,7 +7,7 @@ use std::time::Duration;
  pub fn rate_limit (backend: InMemoryBackend) -> RateLimiter<InMemoryBackend, actix_extensible_rate_limit::backend::SimpleOutput, impl Fn(&actix_web::dev::ServiceRequest) -> std::future::Ready<Result<actix_extensible_rate_limit::backend::SimpleInput, actix_web::Error>> + 'static> 
  
  {
-    let input = SimpleInputFunctionBuilder::new(Duration::from_hours(1), 5)
+    let input = SimpleInputFunctionBuilder::new(Duration::from_secs(1), 3)
             .peer_ip_key()
             .build();
         let middleware = RateLimiter::builder(backend.clone(), input)
