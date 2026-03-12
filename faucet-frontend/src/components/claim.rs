dd0551b6a2b973e680adecd2aaf55e4239606fac
Eccentric healer <healer@eccentrichealer.xyz>
2026-03-12 15:57:45 +0100

    Type casting reformation

--- faucet-frontend/src/components/claim.rs
+++ faucet-frontend/src/components/claim.rs
@@ -2,46 +2,46 @@ use crate::imports::*;
 
 #[component]
 pub fn Claim() -> Element {
-
     let mut status = use_signal(|| format!(""));
-    let mut address = use_signal(||format!(""));
-    let mut hash = use_signal(||None);
-    let mut isfetching = use_signal(||false);
+    let mut address = use_signal(|| format!(""));
+    let mut hash = use_signal(|| None);
+    let mut isfetching = use_signal(|| false);
     let claimer = move |_| async move {
         isfetching.set(true);
-        let url = format!("https://sepolia-faucet.eccentrichealer.xyz/api/claim/{}", address());
+        let url = format!(
+            "https://sepolia-faucet.eccentrichealer.xyz/api/claim/{}",
+            address()
+        );
         let response = reqwest::get(url).await;
         if let Err(e) = response {
-            let error =   e.to_string();
+            let error = e.to_string();
             isfetching.set(false);
-            status.set(format!("error"));
+            status.set("error".to_string());
             return hash.set(Some(error));
         }
         let result = response.unwrap().json::<Request>().await;
         if let Err(e) = result {
             let error = e.to_string();
             isfetching.set(false);
-            status.set(format!("error"));
+            status.set("error".to_string());
             return hash.set(Some(error));
         }
         let result = result.unwrap();
-        if result.status == format!("error") {
+        if result.status == "error" {
+            status.set("error".to_string());
+            hash.set(Some(result.message));
+            isfetching.set(false);
+        } else {
             status.set(result.status);
             hash.set(Some(result.message));
             isfetching.set(false);
         }
-        else {
-            status.set(result.status);
-            hash.set(Some(result.message));
-            isfetching.set(false);
-        }
-
     };
- 
-    rsx!{
-        div { 
+
+    rsx! {
+        div {
             h1 { "WELCOME TO THE CLAIM PAGE" }br {} br {}
-            Link {to: Route::Home, "Home"}br {}
+            Link {to: MyRoute::Home, "Home"}br {}
             label {"enter your address below"}br {}
             input {type: "text", oninput: move |e| address.set(e.value())   }
             br{}
@@ -55,14 +55,14 @@ pub fn Claim() -> Element {
                     "CLAIM"
                  }
             }
-            p { 
+            p {
                 if let Some(hash) = hash() {
                     if status() == "success" {
                         "Claim sucessful" br {}
                         "transaction hash: {hash}"
                     }
                     else {
-                        
+
                         "Error: {hash}"
                     }
                 }
