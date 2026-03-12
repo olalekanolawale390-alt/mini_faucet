720064ff4d065187ca328fa1db9f9114d38f50f0
Eccentric healer <healer@eccentrichealer.xyz>
2026-03-12 18:43:15 +0100

    Add files via upload

--- faucet-frontend/src/components/home.rs
+++ faucet-frontend/src/components/home.rs
@@ -2,15 +2,18 @@ use crate::imports::*;
 
 #[component]
 pub fn Home() -> Element {
-    rsx!{
-        div { 
-            h1 { 
+    rsx! {
+        div {
+            h1 {
                 "WELCOME TO SEPOLIA FAUCET"
              }br {}
-             Link {to: Route::Claim, "Claim page"} br {}
-             button { 
-                onclick: move |_|{ use_navigator().push(Route::NextTime);}, "check next claim time"
-              }
+             Link {to: MyRoute::Claim, "Claim page"} br {}
+             button {
+                onclick: move |_|{ use_navigator().push(MyRoute::NextTime);}, "check next claim time"
+              }br {  }
+              Link{ to: MyRoute::MyBalance, "check balance"} br{}br{}
+              Link{to: MyRoute::FaucetBalance, "Faucet Balance"} br{}br{}
+              Link{to: MyRoute::Contribute, "Contribute"}
 
          }
     }
